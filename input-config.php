ce337ceb6a93b756ef1b68c060a7b1fb2ce9c777
Rofi Anugrah Firdaus <110004805+RofiAnugrah03@users.noreply.github.com>
2022-11-17 12:39:01 +0530

    Add files via upload

---
+++ input-config.php
@@ -0,0 +1,16 @@
+<?php
+    date_default_timezone_set('Asia/Jakarta');
+
+    if(!isset($_SESSION))
+    {
+        include('./header.php');
+        session_start();
+    }
+    
+
+    $mysqli = new mysqli("localhost", "root", "", "kwu_rofiaf");
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }
+?>
